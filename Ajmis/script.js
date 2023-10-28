301ee02b3b7d947c5fbf29f79657faafcee36592
Missriya <60377382+Missriya@users.noreply.github.com>
2023-10-28 20:30:23 +0530

    music

--- Ajmis/script.js
+++ Ajmis/script.js
@@ -2,16 +2,20 @@ $(document).ready(function() {
     const $heart = $("#heart");
     const $modal = $("#modal");
     const $paper = $(".paper");
-
+    const $music = $("#background-music")[0]; // Get the audio element as a DOM object
+    $music.volume = 0.2; // Set the volume to 0.5 (half volume)
     function openModal() {
         $modal.fadeIn(500);
-        $paper.fadeIn(500);
+        $paper.fadeIn(1000);
+        $music.currentTime = 0;
+        $music.play(); // Start playing the background music
     }
 
     function closeModal() {
         $paper.fadeOut(1000, function() {
             $modal.fadeOut(500);
         });
+        $music.pause(); // Pause the background music when closing the modal
     }
 
     $heart.click(openModal);
